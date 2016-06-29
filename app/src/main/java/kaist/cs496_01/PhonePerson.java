420144a81a4c22069d2628b368f813e732451c21
Oortcloud <a3626a@naver.com>
2016-06-29 17:28:30 +0900

    파일 입출력을 이용하여 JSON으로 변환한 연락처 저장

--- app/src/main/java/kaist/cs496_01/PhonePerson.java
+++ app/src/main/java/kaist/cs496_01/PhonePerson.java
@@ -13,7 +13,7 @@ public class PhonePerson {
     private String name;
     private int number;
 
-    public PhonePerson (JSONObject jobj) {
+    public PhonePerson(JSONObject jobj) {
         try {
             this.name = jobj.getString("name");
             this.number = jobj.getInt("number");
@@ -26,5 +26,14 @@ public class PhonePerson {
         nametext.setText(this.name);
         TextView numbertext = (TextView) view.findViewById(R.id.number);
         numbertext.setText(Integer.toString(this.number));
+    }
+
+    public JSONObject toJSON() {
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("name", name);
+            obj.put("number", number);
+        } catch (JSONException e) {}
+        return obj;
     }
 }
--- app/src/main/java/kaist/cs496_01/TabAFragment.java
+++ app/src/main/java/kaist/cs496_01/TabAFragment.java
@@ -1,6 +1,7 @@
 package kaist.cs496_01;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,9 +14,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -24,37 +35,60 @@ import java.util.ArrayList;
 public class TabAFragment extends Fragment {
 
     static JSONAdapter adapter;
+    private final String filename = "phonebook";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getActivity().openFileInput(filename)));
+            String text;
+            String jsonfile="";
+            while ((text=reader.readLine())!=null) {
+                jsonfile+=text;
+            }
+            JSONArray jarray = new JSONArray(jsonfile);
+            ArrayList<PhonePerson> values = new ArrayList<>();
+            for (int i = 0 ; i < jarray.length(); i++) {
+                values.add(new PhonePerson((JSONObject) jarray.get(i)));
+            }
+            adapter = new JSONAdapter(getActivity(), values);
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            File fPhoneBook = new File(getContext().getFilesDir(), filename);
+            ArrayList<PhonePerson> values = new ArrayList<>();
+            adapter = new JSONAdapter(getActivity(), values);
+        } catch (IOException e) {}
+        catch (JSONException e) {}
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        JSONArray jarr = new JSONArray();
+
+        for (int i = 0; i < adapter.getCount(); i++) {
+            PhonePerson iPerson = (PhonePerson)adapter.getItem(i);
+            JSONObject iObj = iPerson.toJSON();
+            jarr.put(iObj);
+        }
+        try {
+            FileOutputStream outputStream = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(jarr.toString().getBytes());
+            outputStream.close();
+        } catch (FileNotFoundException e) {}
+        catch (IOException e) {}
+
+
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ArrayList<PhonePerson> values = new ArrayList<>();
-
-        JSONObject jobj;
-
-        jobj = new JSONObject();
-        try {
-            jobj.put("name", "KIM");
-            jobj.put("number", 123);
-        } catch (JSONException e) {}
-        values.add(new PhonePerson(jobj));
-
-        jobj = new JSONObject();
-        try {
-            jobj.put("name", "LEE");
-            jobj.put("number", 234);
-        } catch (JSONException e) {}
-        values.add(new PhonePerson(jobj));
-
-        jobj = new JSONObject();
-        try {
-            jobj.put("name", "PARK");
-            jobj.put("number", 345);
-        } catch (JSONException e) {}
-        values.add(new PhonePerson(jobj));
-
-        adapter = new JSONAdapter(getActivity(), values);
-
         View view = inflater.inflate(R.layout.tab_phone, null);
         ListView lv = (ListView) view.findViewById(R.id.list);
         lv.setAdapter(adapter);
